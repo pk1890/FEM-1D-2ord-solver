b850828b0032840930d50192c86689a1c6f18fdd
Piotrek Kotara <piotrekkotara@gmail.com>
2019-01-20 15:24:31 +0100

    added sample image

--- ru.py
+++ ru.py
@@ -5,9 +5,10 @@ from scipy.misc import derivative
 from numpy.linalg import linalg
 from functools import reduce
 
+
 def E(i, n):
-    h = 1/n
-    return lambda x: max(0, (1-abs((x-i*h)/(h))))
+    h = LEN/n
+    return lambda x: max(0, (1-abs((-A + x-i*h)/(h))))
 
 def dE(i, n):
     return lambda x: derivative(E(i, n), x, dx = 1e-6)
@@ -17,20 +18,20 @@ def BMatrixElem(i, j, n, fa, fb, fc, beta):
     u = E(i, n)
     dv = dE(j, n)
     du = dE(i, n)
-    I1 = quad((lambda x: fa(x) * du(x) * dv(x)), 0, 1)[0]
-    I2 = quad((lambda x: fb(x) * du(x) * v(x)), 0, 1)[0]
-    I3 = quad((lambda x: fc(x) * u(x) * v(x)), 0, 1)[0]
-    return (-v(0)*u(0)*beta - I1 + I2 + I3)
+    I1 = quad((lambda x: fa(x) * du(x) * dv(x)), A, B, limit=QUAD_LIMIT, points=QUAD_POINTS)[0]
+    I2 = quad((lambda x: fb(x) * du(x) * v(x)), A, B, limit=QUAD_LIMIT, points=QUAD_POINTS)[0]
+    I3 = quad((lambda x: fc(x) * u(x) * v(x)), A, B, limit=QUAD_LIMIT, points=QUAD_POINTS)[0]
+    return (-v(A)*u(A)*beta - I1 + I2 + I3)
 
 def BShift(j, n, fa, fb, fc, shift, beta):
     v = E(j, n)
     dv = dE(j, n)
     u = shift
     du = lambda x: derivative(u, x, dx=1e-6)
-    I1 = quad((lambda x: fa(x) * du(x) * dv(x)), 0, 1)[0]
-    I2 = quad((lambda x: fb(x) * du(x) * v(x)), 0, 1)[0]
-    I3 = quad((lambda x: fc(x) * u(x) * v(x)), 0, 1)[0]
-    return (-v(0)*u(0)*beta - I1 + I2 + I3)
+    I1 = quad((lambda x: fa(x) * du(x) * dv(x)), A, B, limit=QUAD_LIMIT, points=QUAD_POINTS)[0]
+    I2 = quad((lambda x: fb(x) * du(x) * v(x)), A, B, limit=QUAD_LIMIT, points=QUAD_POINTS)[0]
+    I3 = quad((lambda x: fc(x) * u(x) * v(x)), A, B, limit=QUAD_LIMIT, points=QUAD_POINTS)[0]
+    return (-v(A)*u(A)*beta - I1 + I2 + I3)
 
 
 def BMatrix(n, fa, fb, fc, beta):
@@ -44,8 +45,8 @@ def BMatrix(n, fa, fb, fc, beta):
 
 def L(ff, j, n, fa, fb, fc, beta, gamma, n1):
     v = E(j, n)
-    I = quad((lambda x: ff(x) * v(x)), 0, 1)[0]
-    return I - gamma*v(0) - BShift(j, n, fa, fb, fc, (lambda x: n1*E(n,n)(x)), beta)
+    I = quad((lambda x: ff(x) * v(x)), A, B)[0]
+    return I - gamma*v(A) - BShift(j, n, fa, fb, fc, (lambda x: n1*E(n,n)(x)), beta)
 
 def LMatrix(n, f, fa, fb, fc, beta, gamma, n1):
     ML = np.zeros(n)
@@ -58,46 +59,71 @@ def solveGalerkin(fa, fb, fc, ff, beta, gamma, n1, n):
     ML = LMatrix(n, ff, fa, fb, fc, beta, gamma, n1)
     print(MB)
     print(ML)
-    lp = np.linspace(0, 1, 100)
+
 
     res = linalg.solve(MB, ML)
 
-    print(res)
+    res = np.append(res, n1)
 
-    FinalFunc = lambda x: 0
-    for i in range(0, n):
-        FinalFunc = constructFinalFunc(FinalFunc, (lambda x: res[i] * E(i, n)(x)))
+    # print(res)
 
-    
-
-    # FinalFunc = constructFinalFunc(FinalFunc, (lambda x: E(n, n)(x) * n1))
-    
-    plt.plot(lp, np.fromiter(map((lambda x: E(0, n)(x) + E(1, n)(x)), lp), dtype=np.float_))
-    # plt.plot(lp, np.fromiter(map(E(5, 5), lp), dtype=np.float_))
-
-    plt.show()
+    return res
 
 
 
-def constructFinalFunc(f, g):
-    return lambda x: f(x) + g(x)
+
+
+def FinalFunc(n, res, x):
+    val = 0
+    for i in range(0, n+1):
+        val += res[i] * E(i, n)(x)
+    return val
+
+A = -3*np.pi
+B = 4*np.pi
+LEN = B-A
 
 
 a = lambda x: 1
 b = lambda x: 0
-c = lambda x: 0
+c = lambda x: 1
 f = lambda x: 0
 Beta = 0
 Gamma = 0
-N1 = 1.0
+N1 = np.cos(B)
+N = int(40)
 
-solveGalerkin(a, b, c, f, Beta, Gamma, N1, 5)
-# print (list(map(E(2, 10), lp)))
+PLOT_RANGE = [A-0.05*LEN, B+0.05*LEN, -1.5, 5]
+QUAD_LIMIT = 270
+QUAD_POINTS = np.linspace(A, B, N)
 
-# es = [(lambda x: E(i, 5)(x) * res[i]) for i in range(0, 5+1)]
 
 
-# plt.plot(lp, np.fromiter(map(E(2, 10), lp), dtype=np.float_))
-# plt.plot(lp, np.fromiter(map(E(5, 5), lp), dtype=np.float_))
+print("Calculating cos")
+cosinus = solveGalerkin(a, b, c, f, Beta, Gamma, N1, N)
 
-# plt.show()
+# N1 = np.sin(B)
+
+
+# print("Calculating sin")
+# sinus = solveGalerkin(a, b, c, f, Beta, Gamma, N1, N)
+
+a = lambda x: 1
+b = lambda x: 0
+c = lambda x: -1
+f = lambda x: 0
+Beta = 0
+Gamma = 0
+N1 = np.exp(B)
+N = int(40)
+
+
+print("Calculating exp")
+exponent = solveGalerkin(a, b, c, f, Beta, Gamma, N1, N)
+
+lp = np.linspace(A, B, 100)
+# plt.plot(lp, np.fromiter(map((lambda x: FinalFunc(N, sinus, x)), lp), dtype=np.float_))
+plt.plot(lp, np.fromiter(map((lambda x: FinalFunc(N, cosinus, x)), lp), dtype=np.float_))
+plt.plot(lp, np.fromiter(map((lambda x: FinalFunc(N, exponent, x)), lp), dtype=np.float_))
+plt.axis(PLOT_RANGE)
+plt.show()
